32d313e6d6b53d0bb26b328333580afa8ec6bc4c
Franco Caceres Vasquez <franco.caceres@pucp.edu.pe>
2022-07-20 07:33:01 -0500

    #

--- _build/html/_sources/intro.md
+++ _build/html/_sources/intro.md
@@ -1,12 +1,4 @@
-# **Welcome to your Jupyter Book**
+# Machine Learning and Causal Inference
 
-This is a small sample book to give you a feel for how book content is
-structured.
-It shows off a few of the major file types, as well as some sample content.
-It does not go in-depth into any particular topic - check out [the Jupyter Book documentation](https://jupyterbook.org) for more information.
+This Jupyterbook has been created based on the tutorials of the course 14.388 Inference on Causal and Structural Parameters Using ML and AI in the Department of Economics at MIT taught by Professor Victor Chernozukhov. All the scripts were in R and we decided to translate them into Python, so students can manage both programing languages. Jannis Kueck and V. Chernozukhov have also published the original R Codes in Kaggle. In adition, we included tutorials on Heterogenous Treatment Effects Using Causal Trees and Causal Forest from Susan Athey’s Machine Learning and Causal Inference course. We aim to add more empirical examples were the ML and CI tools can be applied using both programming languages.
 
-Check out the content pages bundled with this sample book to see more.
-
-```{tip}
-Some text that gives a useful tip to your readers!
-```
---
+++ _build/jupyter_execute/Python_notebooks/10_pm2_notebook_jannis.py
@@ -0,0 +1,332 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# * Python code replication of:
+# " https://www.kaggle.com/janniskueck/pm2-notebook-jannis "
+# * Created by: Alexander Quispe and Anzony Quispe 
+
+# This notebook contains an example for teaching.
+
+# # Testing the Convergence Hypothesis
+
+# ## Introduction
+
+# We provide an additional empirical example of partialling-out with Lasso to estimate the regression coefficient $\beta_1$ in the high-dimensional linear regression model:
+#   $$
+#   Y = \beta_1 D +  \beta_2'W + \epsilon.
+#   $$
+#   
+# Specifically, we are interested in how the rates  at which economies of different countries grow ($Y$) are related to the initial wealth levels in each country ($D$) controlling for country's institutional, educational, and other similar characteristics ($W$).
+#   
+# The relationship is captured by $\beta_1$, the *speed of convergence/divergence*, which measures the speed at which poor countries catch up $(\beta_1< 0)$ or fall behind $(\beta_1> 0)$ rich countries, after controlling for $W$. Our inference question here is: do poor countries grow faster than rich countries, controlling for educational and other characteristics? In other words, is the speed of convergence negative: $ \beta_1 <0?$ This is the Convergence Hypothesis predicted by the Solow Growth Model. This is a structural economic model. Under some strong assumptions, that we won't state here, the predictive exercise we are doing here can be given causal interpretation.
+# 
+
+# The outcome $Y$ is the realized annual growth rate of a country's wealth  (Gross Domestic Product per capita). The target regressor ($D$) is the initial level of the country's wealth. The target parameter $\beta_1$ is the speed of convergence, which measures the speed at which poor countries catch up with rich countries. The controls ($W$) include measures of education levels, quality of institutions, trade openness, and political stability in the country.
+
+# ## Data analysis
+# 
+
+# We consider the data set GrowthData which is included in the package *hdm*. First, let us load the data set to get familiar with the data.
+
+# In[1]:
+
+
+import hdmpy
+import pandas as pd
+import numpy as np
+import pyreadr
+import math
+import matplotlib.pyplot as plt
+import random
+
+
+# In[2]:
+
+
+# I downloaded the data that the author used
+growth_read = pyreadr.read_r("../data/GrowthData.RData")
+
+# Extracting the data frame from rdata_read
+growth = growth_read[ 'GrowthData' ]
+
+
+# We determine the dimension of our data set.
+
+# In[3]:
+
+
+growth.shape
+
+
+# The sample contains $90$ countries and $63$ controls. Thus $p \approx 60$, $n=90$ and $p/n$ is not small. We expect the least squares method to provide a poor estimate of $\beta_1$.  We expect the method based on partialling-out with Lasso to provide a high quality estimate of $\beta_1$.
+
+# To check this hypothesis, we analyze the relation between the output variable $Y$ and the other country's characteristics by running a linear regression in the first step.
+
+# In[4]:
+
+
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
+
+# In[5]:
+
+
+# We create the main variables
+y = growth['Outcome']
+X = growth.drop('Outcome', 1)
+
+
+# In[6]:
+
+
+# OLS regression
+reg_ols  = sm.OLS(y, X).fit()
+print(reg_ols.summary())
+
+
+# In[7]:
+
+
+# output: estimated regression coefficient corresponding to the target regressor
+est_ols = reg_ols.summary2().tables[1]['Coef.']['gdpsh465']
+
+# output: std. error
+std_ols = reg_ols.summary2().tables[1]['Std.Err.']['gdpsh465']
+
+# output: 95% confidence interval
+lower_ci = reg_ols.summary2().tables[1]['[0.025']['gdpsh465']
+upper_ci = reg_ols.summary2().tables[1]['0.975]']['gdpsh465']
+
+
+# ## Summarize OLS results
+
+# In[8]:
+
+
+table_1 = np.zeros( (1, 4) )
+
+table_1[0,0] = est_ols  
+table_1[0,1] = std_ols   
+table_1[0,2] = lower_ci
+table_1[0,3] = upper_ci    
+
+
+table_1_pandas = pd.DataFrame( table_1, columns = [ "Estimator","Std. Error", "lower bound CI", "upper bound CI"  ])
+table_1_pandas.index = [ "OLS" ]
+table_1_html
+
+
+# <!-- html table generated in R 3.6.3 by xtable 1.8-4 package -->
+# <!-- Tue Jan 19 10:23:32 2021 -->
+# <table border=1>
+# <tr> <th>  </th> <th> estimator </th> <th> standard error </th> <th> lower bound CI </th> <th> upper bound CI </th>  </tr>
+#   <tr> <td align="right"> OLS </td> <td align="right"> -0.009 </td> <td align="right"> 0.030 </td> <td align="right"> -0.071 </td> <td align="right"> 0.052 </td> </tr>
+#    </table>
+
+# Least squares provides a rather noisy estimate (high standard error) of the
+# speed of convergence, and does not allow us to answer the question
+# about the convergence hypothesis since the confidence interval includes zero.
+# 
+# In contrast, we can use the partialling-out approach based on lasso regression ("Double Lasso").
+
+# In[11]:
+
+
+# Create main variables
+Y = growth['Outcome']
+W = growth.drop(['Outcome','intercept', 'gdpsh465'], 1 )
+D = growth['gdpsh465']
+
+
+# ## Method 1 - Using Sklearn
+
+# In[12]:
+
+
+from sklearn import linear_model
+
+# Seat values for Lasso
+lasso_model = linear_model.Lasso( alpha = 0.00077 )
+r_Y = Y - lasso_model.fit( W, Y ).predict( W )
+r_Y = r_Y.rename('r_Y')
+
+# Part. out d
+r_D = D - lasso_model.fit( W, D ).predict( W )
+r_D = r_D.rename('r_D')
+
+# ols 
+partial_lasso_fit = sm.OLS(r_Y, r_D).fit()
+
+est_lasso = partial_lasso_fit.summary2().tables[1]['Coef.']['r_D']
+std_lasso = partial_lasso_fit.summary2().tables[1]['Std.Err.']['r_D']
+lower_ci_lasso = partial_lasso_fit.summary2().tables[1]['[0.025']['r_D']
+upper_ci_lasso = partial_lasso_fit.summary2().tables[1]['0.975]']['r_D']
+
+
+# In[14]:
+
+
+# Regress residuales
+partial_lasso_fit = sm.OLS(r_Y, r_D).fit()
+partial_lasso_est = partial_lasso_fit.summary2().tables[1]['Coef.']['r_D']
+
+print( f"Coefficient for D via partialling-out using lasso {partial_lasso_est}" )
+
+
+# In[15]:
+
+
+# output: estimated regression coefficient corresponding to the target regressor
+est_lasso = partial_lasso_fit.summary2().tables[1]['Coef.']['r_D']
+
+# output: std. error
+std_lasso = partial_lasso_fit.summary2().tables[1]['Std.Err.']['r_D']
+
+# output: 95% confidence interval
+lower_ci_lasso = partial_lasso_fit.summary2().tables[1]['[0.025']['r_D']
+upper_ci_lasso = partial_lasso_fit.summary2().tables[1]['0.975]']['r_D']
+
+
+# ## Summary LASSO results
+# 
+
+# Finally, let us have a look at the results.
+
+# In[16]:
+
+
+table_2 = np.zeros( (1, 4) )
+
+table_2[0,0] = est_lasso  
+table_2[0,1] = std_lasso   
+table_2[0,2] = lower_ci_lasso
+table_2[0,3] = upper_ci_lasso    
+
+
+table_2_pandas = pd.DataFrame( table_2, columns = [ "Estimator","Std. Error", "lower bound CI", "upper bound CI"  ])
+table_2_pandas.index = [ "LASSO" ]
+table_2_pandas
+
+
+# In[17]:
+
+
+table_3 = table_1_pandas.append(table_2_pandas)
+table_3
+
+
+# The least square method provides a rather noisy estimate of the speed of convergence. We can not answer the question if poor countries grow faster than rich countries. The least square method does not work when the ratio $p/n$ is large.
+# 
+# In sharp contrast, partialling-out via Lasso provides a more precise estimate. The Lasso based point estimate is $-5\%$ and the $95\%$ confidence interval for the (annual) rate of convergence $[-7.8\%,-2.2\%]$ only includes negative numbers. This empirical evidence does support the convergence hypothesis.
+
+# <table border="1" class="dataframe">
+#   <thead>
+#     <tr style="text-align: right;">
+#       <th></th>
+#       <th>Estimator</th>
+#       <th>Std. Error</th>
+#       <th>lower bound CI</th>
+#       <th>upper bound CI</th>
+#     </tr>
+#   </thead>
+#   <tbody>
+#     <tr>
+#       <th>OLS</th>
+#       <td>-0.009378</td>
+#       <td>0.029888</td>
+#       <td>-0.070600</td>
+#       <td>0.051844</td>
+#     </tr>
+#     <tr>
+#       <th>LASSO</th>
+#       <td>-0.047747</td>
+#       <td>0.017705</td>
+#       <td>-0.082926</td>
+#       <td>-0.012567</td>
+#     </tr>
+#   </tbody>
+# </table>
+
+# ## Method 2 - HDMPY
+
+# In[19]:
+
+
+res_Y = hdmpy.rlasso( W, Y, post=True ).est['residuals']
+res_D = hdmpy.rlasso( W, D, post=True ).est['residuals']
+
+r_Y = pd.DataFrame(res_Y, columns=['r_Y'])
+r_D = pd.DataFrame(res_D, columns=['r_D'])
+
+
+# In[20]:
+
+
+# OLS regression
+reg_ols  = sm.OLS(r_Y, r_D).fit()
+print(reg_ols.summary())
+
+
+# In[21]:
+
+
+# output: estimated regression coefficient corresponding to the target regressor
+est_lasso = reg_ols.summary2().tables[1]['Coef.']['r_D']
+
+# output: std. error
+std_lasso = reg_ols.summary2().tables[1]['Std.Err.']['r_D']
+
+# output: 95% confidence interval
+lower_ci_lasso = reg_ols.summary2().tables[1]['[0.025']['r_D']
+upper_ci_lasso = reg_ols.summary2().tables[1]['0.975]']['r_D']
+
+
+# In[22]:
+
+
+table_3 = np.zeros( (1, 4) )
+
+table_3[0,0] = est_lasso   
+table_3[0,1] = std_lasso    
+table_3[0,2] = lower_ci_lasso 
+table_3[0,3] = upper_ci_lasso     
+
+
+table_3_pandas = pd.DataFrame( table_3, columns = [ "Estimator","Std. Error", "lower bound CI", "upper bound CI"  ]) 
+table_3_pandas.index = [ "LASSO" ]
+table_3_pandas
+
+
+# ## Method 3 - HDMPY Direct
+
+# In[23]:
+
+
+lasso_direct = hdmpy.rlassoEffect(x=W, y=Y, d=D, method="partialling out")
+
+
+# In[24]:
+
+
+est_lasso = lasso_direct["coefficients"]
+std_lasso = lasso_direct["se"]
+lower_ci_lasso = est_lasso - 1.96*std_lasso
+upper_ci_lasso = est_lasso + 1.96*std_lasso
+
+
+# In[25]:
+
+
+table_4 = np.zeros( (1, 4) )
+
+table_4[0,0] = est_lasso   
+table_4[0,1] = std_lasso    
+table_4[0,2] = lower_ci_lasso 
+table_4[0,3] = upper_ci_lasso     
+
+
+table_4_pandas = pd.DataFrame( table_4, columns = [ "Estimator","Std. Error", "lower bound CI", "upper bound CI"  ]) 
+table_4_pandas.index = [ "LASSO_direct" ]
+table_4_pandas
+
